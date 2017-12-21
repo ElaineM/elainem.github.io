38ccb310c6ad4d0d6cfec06ff404c11caa95d30d
ElaineM <elaine.matthias@gmail.com>
2017-12-21 04:40:08 -0500

    final update

--- p5Maze/script.js
+++ p5Maze/script.js
@@ -1,9 +1,16 @@
-var myArray = [];
-var option = 1;
+var option = 0;
+var position = "";
+var stroke = 20;
 
+//reloads the page when "start over" is clicked
+$("#reload").click(function() {
+	location.reload();
+});
+
+//creates the maze shape
 function rightShape(x,y) {
 	beginShape();
-	strokeWeight(20);
+	strokeWeight(10);
 	vertex(180+x,0+y);
 	vertex(180+x,20+y);
 	vertex(160+x,20+y);
@@ -29,7 +36,7 @@ function rightShape(x,y) {
 
 function leftShape(x,y) {
 	beginShape();
-	strokeWeight(20);
+	strokeWeight(10);
 	vertex(0+x,20+y);
 	vertex(20+x,20+y);
 	vertex(20+x,40+y);
@@ -53,48 +60,115 @@ function leftShape(x,y) {
 	endShape();
 	}
 
+// P5 function that creates the canvas
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	// console.log(myArray);
 }
 
+// P5 function that draws on the canvas
 function draw(){
-	
-		if (option == 1) {
-			if (keyCode == UP_ARROW) {
-				leftShape(100,100);
-				console.log("1");
-			
-			} else if (keyCode == DOWN_ARROW) {
-				rightShape(300,100);
-				console.log("2")
-		
-			} 
-		} else if (option == 2) {
-			if (keyCode == UP_ARROW) {
-				leftShape(200,200);
-			
-			} else if (keyCode == DOWN_ARROW) {
-				rightShape(600,200);
-		
-			} 
-		} else if (option == 3) {
-			if (keyCode == UP_ARROW) {
-				leftShape(300,300);
-			
-			} else if (keyCode == DOWN_ARROW) {
-				rightShape(900,300);
-			
-			} 
-		}
 
-		function mousePressed() {
-			option++;
-			console.log(option);
-		}
-	// }
+		//grid lines
+		line(0,0,0,600);
+		line(200,0,200,600);
+		line(400,0,400,600);
+		line(600,0,600,600);
+		line(0,0,600,0);
+		line(0,200,600,200);
+		line(0,400,600,400);
+		line(0,600,600,600);
 }
-	
+
+function keyPressed() {
+		if (option == 0) {
+			if (keyCode == UP_ARROW) {
+				leftShape(0,0);
+				position = "left";
+		
+			} else if (keyCode == DOWN_ARROW) {
+				rightShape(0,0);
+				position = "right";
+		
+			} 
+		} else if (option == 1) {
+			if (position == "left") {
+					if (keyCode == UP_ARROW) {
+						rightShape(200,0);
+						position = "rightTwo";
+					} else if (keyCode == DOWN_ARROW) {
+						rightShape(0,200);
+						position == "rightFour";
+						console.log("test");
+					}
+			} else if (position == "right") {
+					if (keyCode == UP_ARROW) {
+						leftShape(0,200);
+						position = "leftFour";
+					} else if (keyCode == DOWN_ARROW) {
+						leftShape(200,0);
+						position == "leftTwo";
+					}
+				}
+
+		} else if (option == 2) {
+					if (position == "rightTwo") {
+						if (keyCode == UP_ARROW) {
+							leftShape(400,0);
+							position = "leftThree";
+						} else if (keyCode == DOWN_ARROW) {
+							//game over
+						} 
+					} else if (position == "rightFour") {
+						if (keyCode == UP_ARROW) {
+							leftShape(200,200);
+							position = "leftFive";
+						} else if (keyCode == DOWN_ARROW) {
+							leftShape(0,400);
+							position = "rightSeven";
+						} 
+					}
+		} else if (option == 3) {
+				if (position == "leftThree") {
+					if (keyCode == DOWN_ARROW) {
+						rightShape(400,200);
+						position = "rightSix";
+				} else if (keyCode == UP_ARROW) {
+						alert("Game Over");
+				}
+			}
+
+		} else if (option == 4) {
+				if (position == "rightSix") {
+					if (keyCode == UP_ARROW) {
+						leftShape(400,400);
+						position = "leftNine";
+
+				} else if (keyCode == DOWN_ARROW) {
+						rightShape(200,200);
+						position = "leftFive";
+					}
+				}
+			
+		} else if (option == 5) {
+				if (position == "leftNine") {
+					if (keyCode == UP_ARROW) {
+						alert("You win!");
+				} else if (keyCode == DOWN_ARROW) {
+						alert("You Loose!");
+					}
+				}
+		} else { }
+		//for testing only
+		$("#gridPosition").html(option + " , " + position);
+		
+		//incrementer that moves the user forward in the maze
+		option++;
+	}
+
+
+
+	// OLD PRACTICE STUFF || from P5 reference
 // function drawCircles(x,y){
 // 	ellipse(x,y-25,10,10);
 // 	// ellipse(x,y,10,10);
